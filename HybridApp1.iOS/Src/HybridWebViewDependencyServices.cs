49e4dab5b62c29fe1de2227d27be3de28d8e47d2
ROOT\arumugaperumals <arumugaperumals@syncfusion.com>
2017-12-29 17:42:33 +0530

    Added iOS project.

---
+++ HybridApp1.iOS/Src/HybridWebViewDependencyServices.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using WebKit;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(HybridApp1.iOS.HybridWebViewDependencyServices))]
+namespace HybridApp1.iOS
+{
+    public class HybridWebViewDependencyServices : IHybridWebViewDependencyServices
+    {
+        public void SetLabel(object nativeObject, string value)
+        {
+			(nativeObject as WKWebView).EvaluateJavaScript("setLabel('" + value + "')",null);
+        }
+    }
+}
